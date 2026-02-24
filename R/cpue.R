aad593affb4beb05665d1e2d5702e95ae77b60bf
Desgagnés <Mathieu.Desgagnes@dfo-mpo.gc.ca>
2026-02-24 12:55:34 -0500

    typo

--- R/cpue.R
+++ R/cpue.R
@@ -9,7 +9,7 @@
 #'
 #' @examples
 #' cpue(100,10)
-#' cpue(100, 10, gear_fator=0.5)
+#' cpue(100, 10, gear_factor=0.5)
 #'
 cpue <- function(catch, effort, gear_factor = 1) {
   raw_cpue <- catch / effort

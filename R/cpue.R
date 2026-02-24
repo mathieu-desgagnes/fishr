063e91a591b0bb214d099bc680e84bb34ba1d6d9
Desgagnés <Mathieu.Desgagnes@dfo-mpo.gc.ca>
2026-02-24 12:49:56 -0500

    description générale, liscence, et documentation de la fonction cpue()

--- R/cpue.R
+++ R/cpue.R
@@ -1,3 +1,16 @@
+#' Calculate something
+#'
+#' @param catch Numerci vector of catch
+#' @param effort numeric
+#' @param gear_factor Numeric
+#'
+#' @returns someting vector of numeric
+#' @export
+#'
+#' @examples
+#' cpue(100,10)
+#' cpue(100, 10, gear_fator=0.5)
+#'
 cpue <- function(catch, effort, gear_factor = 1) {
   raw_cpue <- catch / effort
 

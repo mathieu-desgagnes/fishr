b3b071e0ed4047a339e6d553ec42bdc034e99c9d
Desgagnés <Mathieu.Desgagnes@dfo-mpo.gc.ca>
2026-02-25 12:59:04 -0500

    add method argument

--- R/cpue.R
+++ R/cpue.R
@@ -3,6 +3,7 @@
 #' @param catch Numerci vector of catch
 #' @param effort numeric
 #' @param gear_factor Numeric
+#' @param method Character; one of "ration" or "log"
 #' @param verbose logical
 #'
 #' @returns someting vector of numeric
@@ -16,12 +17,21 @@ cpue <- function(
   catch,
   effort,
   gear_factor = 1,
+  method = c("ratio", "log"),
   verbose = getOption("fishr.verbose", default = FALSE)
 ) {
   if (verbose) {
     message("Processing ", length(catch), " records")
   }
-  raw_cpue <- catch / effort
+
+  method <- match.arg(method) # seulement les valeurs dans l'entête de fonction
+
+  raw_cpue <- switch(
+    # par de problème
+    method,
+    ratio = catch / effort,
+    log = log(catch / effort)
+  )
 
   raw_cpue * gear_factor
 }

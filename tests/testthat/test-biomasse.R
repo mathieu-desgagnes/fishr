32ae65a255387d6df1975bfbe1c9bfe96eb61a8c
Desgagnés <Mathieu.Desgagnes@dfo-mpo.gc.ca>
2026-02-25 14:32:33 -0500

    test de biomasse

--- tests/testthat/test-biomasse.R
+++ tests/testthat/test-biomasse.R
@@ -12,3 +12,9 @@ test_that("biomasse warns when catch and effort lengths differ", {
 
   expect_no_warning(biomass_index(100, 10))
 })
+
+test_that("biomasse avec différente options de verbose", {
+  withr::local_options(fishr.verbose = TRUE)
+
+  expect_snapshot(biomass_index(cpue = 5, area_swept = 100))
+})
